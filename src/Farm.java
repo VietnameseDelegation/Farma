0b76c0788a48a558d79978694f4b3518a1f82b58
VietnameseDelegation <161819565+VietnameseDelegation@users.noreply.github.com>
2024-03-11 08:12:32 +0100

    added class Sheep that extends Animal

--- src/Farm.java
+++ src/Farm.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
+
 public class Farm {
+    ArrayList<Flower> flowerPlantation = new ArrayList<>();
+    // zasadit, zavlažovat a sklízet
+    public void harvest(){
+
+    }
+    public void plant(Flower flower){
+        flowerPlantation.add(flower);
+    }
 
 
 
